b1838d2d37b84db70ae5e25267dbfbfac1e46ab7
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-25 23:24:54 +0200

    Prepared statements
    video: 26:04

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
-echo "hello";
+echo "hello <br>";
 	
 	$host = 'localhost';
 	$user = 'root';
 	$pass = 'root';
 	$dbname ='pdoposts';
 	// set DSN
-	 $dsn = 'mysql:host='.$host.';dbname='.$dbname;
+	$dsn = 'mysql:host='.$host.';dbname='.$dbname;
 
 	// create PDO instance (connections)
 	$pdo = new PDO($dsn, $user, $pass);
 
 	//set an attribute this way we do not need to indicate the atribute of fetch each time we need it. Take in mind that this way you "ask" for an object.
 	//Used on OPTION 03
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+	/* ****************************************************
+			NORMAL WAY TO CONNECT
+	*****************************************************/
 
 	#PRDO
 	
-	$stmt = $pdo->query('SELECT * FROM posts');
+	//$stmt = $pdo->query('SELECT * FROM posts');
 
 	// OPTION 01 - as an associative array
-//	while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-//		echo $row['title'].'<br>';
-//	}
+	//	while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+	//		echo $row['title'].'<br>';
+	//	}
 	
 	// OPTION 02 
 	//while( $row = $stmt->fetch(PDO::FETCH_OBJ)){
@@ -30,6 +34,34 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 	//}
 
 	// OPTION 03 - with attribute
-	while( $row = $stmt->fetch()){
-			echo $row->title . '<br>';
+	//while( $row = $stmt->fetch()){
+	//		echo $row->title . '<br>';
+	//}
+	
+	
+	/* ****************************************************
+			CONNECT WITH PREPARED STATEMENTS
+	*****************************************************/
+	
+	# prepare & execute
+	# separes the instrution from  the actual data
+	
+	# unsafe way to do it. 
+	#$sql = "SELECT * FROM posts WHERE author = '$author'";
+	
+	// FETCH MULTIPLE POSTS
+	
+	// user input
+	$author = 'Brad';
+
+	//positional parameters
+	$sql = "SELECT * FROM posts WHERE author = ?";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$author]);
+	$posts = $stmt->fetchAll();
+
+	// var_dump($posts);
+	foreach ($posts as $post) 
+	{
+		echo $post->title . '<br>';
 	}
