927a0edcc52da9961326c31167f5e312c576533d
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-25 22:56:34 +0200

    OPTION 03

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ echo "hello";
 	$pdo = new PDO($dsn, $user, $pass);
 
 	//set an attribute this way we do not need to indicate the atribute of fetch each time we need it. Take in mind that this way you "ask" for an object.
-	//Used on OPTION 02
-//$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	//Used on OPTION 03
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
 	#PRDO
 	
@@ -24,7 +24,12 @@ echo "hello";
 //		echo $row['title'].'<br>';
 //	}
 	
-	// OPTION 02 - with attribute
-	while( $row = $stmt->fetch(PDO::FETCH_OBJ)){
+	// OPTION 02 
+	//while( $row = $stmt->fetch(PDO::FETCH_OBJ)){
+	//		echo $row->title . '<br>';
+	//}
+
+	// OPTION 03 - with attribute
+	while( $row = $stmt->fetch()){
 			echo $row->title . '<br>';
 	}
