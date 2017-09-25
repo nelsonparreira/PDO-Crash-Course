8abe9a29ce113cfea8a87bf8866b820f74808f86
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-25 22:54:41 +0200

    OPTION 02
    Video: 17:52

--- index.php
+++ index.php
@@ -11,12 +11,20 @@ echo "hello";
 	// create PDO instance (connections)
 	$pdo = new PDO($dsn, $user, $pass);
 
+	//set an attribute this way we do not need to indicate the atribute of fetch each time we need it. Take in mind that this way you "ask" for an object.
+	//Used on OPTION 02
+//$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
 	#PRDO
 	
 	$stmt = $pdo->query('SELECT * FROM posts');
 
-	//as an associative array
-	while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-		echo $row['title'].'<br>';
-	}
+	// OPTION 01 - as an associative array
+//	while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+//		echo $row['title'].'<br>';
+//	}
 	
+	// OPTION 02 - with attribute
+	while( $row = $stmt->fetch(PDO::FETCH_OBJ)){
+			echo $row->title . '<br>';
+	}
